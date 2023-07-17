827d20f2ab9cf0176e760d476400e6050cfb321e
jumvribeiro <juliamvribeiro@gmail.com>
2023-07-17 19:42:32 -0300

    test validator e js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import validator from './validator.js';
 
+const btnCard= document.getElementById("btnValidate"); // atribui o elemento do botão a uma const chamda "btnCard"
+const alertCard= document.getElementById("card-result"); // atribui a uma const chamada "alertCard"
+
+btnCard.addEventListener("click", cardValidator); // um evento de clique ao botão com o ID "btnCard", quando o botão é clicado, a função "cardValidator" será executada.
+
+function cardValidator() {
+
+  const creditCardNumber= document.getElementById("number").value; // pega o valor do num do cartão
+  const finalResult= validator.isValid(creditCardNumber); // vai linkar com a formula do validator.js
+  const maskedNumbers= validator.maskify(creditCardNumber); // vai linkar com a formula do validator.js
+
+
+  if (creditCardNumber === "") {
+    return alertCard.innerText= "Digite o número do seu cartão"; // verificação pra ver se o campo 'creditCardNumber" está vazio. Se estiver vazio, defini o texto "digite o num do cartão". Garante que o usuário insira um num de cartão antes de prosseguir com a validação.
+  }
+  else if (creditCardNumber.length <= 13) {
+    return alertCard.innerText= "Favor verificar o número do seu cartão";
+  }
+  else if (finalResult === true) {
+    return alertCard.innerText= "Seu cartão de número" + maskedNumbers + "está válido. Estamos processando sua compra!";
+  }
+  else {
+    return alertCard.innerText= "Seu cartão de número" + maskedNumbers + " não está válido. Verifique o número novamente.";
+  }
+}
+
+// código para obter referências aos elementos específicos do seu documento HTML usando os IDs correspondentes
+
+const cardNumber= document.getElementById("number");
+const cardName= document.getElementById("name");
+const cardExpiration= document.getElementById("expiration-date");
+const cardCvv= document.getElementById("cvv");
+
+//"keyup" fornece uma maneira de capturar as ações do usuário em tempo real enquanto ele digita ou interage com elementos de entrada de texto ou outros elementos interativos.
+
+cardNumber.addEventListener("keyup", cloneCardNumber);
+cardName.addEventListener("keyup", cloneCardName);
+cardExpiration.addEventListener("keyup", cloneCardExpiration);
+cardCvv.addEventListener("keyup", cloneCardCvv);
+
+function cloneCardNumber () {
+  const value= document.getElementById ("number").value;
+  const mask= validator.maskify(value);
+  
+  document.getElementById("card-number").value= mask;
+}
+  
+function cloneCardName () {
+  const value= document.getElementById("name").value;
+  
+  document.getElementById("card-name").value= value;
+}
+  
+function cloneCardExpiration () {
+  const value= document.getElementById("expiration-date").value;
+  
+  document.getElementById("card-expiration").value= value;
+}
+  
+function cloneCardCvv () {
+  const value= document.getElementById("cvv")
+  
+  document.getElementById("card-cvv").value= value;
+}
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
 const validator = {
-  // ...
+  
+  isValid (creditCardNumber) { // valida o número do cartão com base no algoritmo Luhn (mod. 10)
+
+    const newArray = []; // array vazio para receber os dados do usuário 
+    for(let i = 0; i < creditCardNumber.length; i++) { // cria a var i = 0, verifica se i < comp. do num do cartão
+      const numCard = parseInt(creditCardNumber[i]); // parseInt() é para converter uma string em um número inteiro. Ela recebe como argumento uma string que representa um número e retorna o valor num correspondente. 
+      newArray.push(numCard) // faz com que cada número convertido do cartão ao array newArray, resultando em um array que contém os números do cartão na mesma ordem.
+    }
+
+    const cardNumReverse = newArray.reverse(); // reverse() é utilizado em arrays para inverter a ordem dos elementos. Ele modifica o próprio array, invertendo os elementos em suas posições originais.
+
+    // proximos passos:
+    // - encontrar os nums que estão nas posições pares
+    // - somar esses nums
+    // - se a soma der > 9, pegar esse resultado e subtrair 9
+
+    for (let i = 0; i < cardNumReverse.length; i++) { // var i = 0 e check se o resultado da expressão (i + 1) % 2 é igual a 0.
+      if ((i + 1) % 2 === 0) { // % verificar se os nums são pares no array cardNumReverse
+        cardNumReverse[i] = cardNumReverse[i] * 2; // multiplicar por 2 os dígitos em posições pares
+      }
+      if (cardNumReverse[i] >= 10) {   //ajustar os valores dos dígitos que se tornam maiores ou iguais a 10 após a multiplicação por 2, subtraindo 9 para obter um novo valor entre 1 e 9.
+        cardNumReverse[i] = cardNumReverse[i] - 9;
+      }
+    }
+
+    // proximos passos:
+    // - somar todos os nums
+    // - verificar se o result é multiplo de 10; se for, o cartão é válido e se não, o cartão é inválido
+
+    let finalNum = 0 // utilizada para armazenar a soma dos elementos do array cardNumReverse
+    for (let i = 0; i < cardNumReverse.length; i++) { //percorre cd elemento do array cardNumReverse e guarda a soma dos elementos na var finalNum. Depois, a var finalNum terá o resultado da soma dos elementos do array 
+      finalNum = finalNum + cardNumReverse[i]
+    }
+    if (finalNum % 10 === 0) { // verifica se a soma dos dígitos do num cartão é múltiplo de 10 para determinar a sua validade
+      return true
+    } else {
+      return false
+    }
+  },
+
+  maskify (creditCardNumber) { //mascarar o número do cartão, substituindo todos os dígitos, exceto os últimos 4
+    const maskNumber = []; // array vazio pra receber num do cartão 
+    for (let i = 0; i < creditCardNumber.length; i++) { 
+      if(i < creditCardNumber.length - 4) { // verifica se o caractere atual está dentro dos últimos 4 dígitos do número do cartão
+        maskNumber.push("#");
+      } else {
+        maskNumber.push(creditCardNumber[i])
+      }
+    }
+    const masked = maskNumber.join("");
+    return masked;
+  }
 };
 
 export default validator;
